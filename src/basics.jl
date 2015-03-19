45cf532ff6d4bed67506d371db59dfa37d7a500f
Mike Innes <mike.j.innes@gmail.com>
2015-03-19 00:59:52 +0000

    nicer dir browser

--- src/basics.jl
+++ src/basics.jl
@@ -55,12 +55,12 @@ status(s) = reskey(:status, s)
 # Error handling
 
 mux_css = """
-  <style>
   body { font-family: sans-serif; padding:50px; }
   .box { background: #fcfcff; padding:20px; border: 1px solid #ddd; border-radius:5px; }
   pre { line-height:1.5 }
+  a { text-decoration:none; color:#225; }
+  a:hover { color:#336; }
   u { cursor: pointer }
-  </style>
   """
 
 error_phrases = ["Looks like someone needs to pay their developers more."
@@ -78,7 +78,7 @@ function basiccatch(app, req)
     app(req)
   catch e
     io = IOBuffer()
-    println(io, mux_css)
+    println(io, "<style>", mux_css, "</style>")
     println(io, "<h1>Internal Error</h1>")
     println(io, "<p>$(error_phrases[rand(1:length(error_phrases))])</p>")
     println(io, "<pre class=\"box\">")
--- src/examples/files.jl
+++ src/examples/files.jl
@@ -18,13 +18,6 @@ fileresponse(f) = @d(:file => f,
                      :body => open(readbytes, f),
                      :headers => fileheaders(f))
 
-dirresponse(f) =
-  html(head(),
-       body(h2("Files"),
-            div(table([tr(td(a(@d(:href=>"$x/"), x)),
-                          td(string(filesize(joinpath(f, x)))))
-                       for x in ["..", readdir(f)...]]))))
-
 fresp(f) =
   isfile(f) ? fileresponse(f) :
   isdir(f) ?  dirresponse(f) :
@@ -33,3 +26,22 @@ fresp(f) =
 files(root, dirs = true) =
   branch(req -> validpath(root, joinpath(req[:path]...), dirs=dirs),
          req -> fresp(joinpath(root, req[:path]...)))
+
+# Directories
+
+files_css = """
+  table { width:100%; border-radius:5px; }
+  td { padding: 5px; }
+  tr:nth-child(odd) { background: #f4f4ff; }
+  .size { text-align: right; }
+  """
+
+filelink(root, f) =
+  a(@d(:href=>"$f/"), isfile(joinpath(root, f)) ? f : "$f/")
+
+dirresponse(f) =
+  html(head(Hiccup.style([mux_css, files_css])),
+       body(h2("Files"),
+            div(".box", table([tr(td(".file", filelink(f, x)),
+                                  td(".size", string(filesize(joinpath(f, x)))))
+                               for x in ["..", readdir(f)...]]))))

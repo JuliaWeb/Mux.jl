53c6cda5f7d9b3a3261b9cdfa91ab1f2c8a028d6
sarvjeet <sssarvjeet27@gmail.com>
2017-04-01 05:01:41 +0530

    Query based routing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,16 @@ using Lazy
 using HTTP
 
 @test Mux.notfound()(d())[:status] == 404
-
+d1 = Dict("one"=> "1", "two"=> "2")
+d2 = Dict("one"=> "1", "two"=> "")
 # Test basic server
 @app test = (
   Mux.defaults,
   page("/",respond("<h1>Hello World!</h1>")),
   page("/about", respond("<h1>Boo!</h1>")),
   page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
+  query(d1, respond("<h1>query1</h1>")),
+  query(d2, respond("<h1>query2</h1>")),
   Mux.notfound())
 serve(test)
 @test String(HTTP.get("http://localhost:8000").body) ==
@@ -30,3 +33,12 @@ function f()
 end
 
 @test f() == nothing
+
+# Query based routing
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1&two=2")) == "<h1>query1</h1>"
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1")) != "<h1>query1</h1>"
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1&two=2&sarv=boo")) != "<h1>query1</h1>"
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1")) != "<h1>query2</h1>"
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1&two=56")) == "<h1>query2</h1>"
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1&two=hfjd")) == "<h1>query2</h1>"
+@test Requests.text(Requests.get("http://localhost:8000/dum?one=1&two=2&sarv=boo")) != "<h1>query2</h1>"

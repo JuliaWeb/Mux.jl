b8419df8774c0ee14052320d22bab41b653d0c01
Pietro Vertechi <pietro.vertechi@protonmail.com>
2019-03-23 12:13:24 +0000

    Fixes for HTTP 0.8 (#86)
    * fixes
    
    * missed one
    
    * more fixes
    
    * rename websocket handler function
    
    * serve asynchronously
    
    * bump websockets dependency

--- src/server.jl
+++ src/server.jl
@@ -1,6 +1,6 @@
 using HTTP.Servers, Lazy, Sockets
 
-import HTTP.HandlerFunction
+import HTTP.RequestHandlerFunction
 import Base.Meta.isexpr
 import WebSockets
 
@@ -39,30 +39,27 @@ function mk_response(d::Dict)
 end
 
 function http_handler(app::App)
-  handler = HandlerFunction((req) -> mk_response(app.warez(req)))
+  handler = RequestHandlerFunction((req) -> mk_response(app.warez(req)))
   # handler.events["error"]  = (client, error) -> println(error)
   # handler.events["listen"] = (port)          -> println("Listening on $port...")
   return handler
 end
 
 function ws_handler(app::App)
-  handler = WebSockets.WebsocketHandler((req, client) -> mk_response(app.warez((req, client))))
+  handler = WebSockets.WSHandlerFunction((req, client) -> mk_response(app.warez((req, client))))
   return handler
 end
 
 const default_port = 8000
 const localhost = ip"0.0.0.0"
 
-function serve(s::Server, host = localhost, port = default_port; kws...)
-  @async @errs HTTP.serve(s, host, port; kws...)
+function serve(h::App, host = localhost, port = default_port; kws...)
+  @async @errs HTTP.serve(http_handler(h), host, port; kws...)
 end
 
-serve(s::Server, port::Integer) = serve(s, localhost, port)
-
-serve(h::App, args...; kws...) =
-    serve(Server(http_handler(h)), args...; kws...)
+serve(h::App, port::Integer) = serve(h, localhost, port)
 
 serve(h::App, w::App, host = localhost, port = default_port) =
-    WebSockets.serve(WebSockets.ServerWS(http_handler(h), ws_handler(w)), host, port)
+    @async @errs WebSockets.serve(WebSockets.ServerWS(http_handler(h), ws_handler(w)), host, port)
 
 serve(h::App, w::App, port::Integer) = serve(h, w, localhost, port)

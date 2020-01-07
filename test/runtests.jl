ff1519257d2b2fe9b26bdd8d6a7abae4b666053f
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-01-07 11:25:19 -0600

    Add tests for prod_defaults

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,3 +46,24 @@ end
 @test String(HTTP.get("http://localhost:8000/dum?one=1&two=hfjd").body) ==
             "<h1>query2</h1>"
 @test_throws StatusError String(HTTP.get("http://localhost:8000/dum?one=1&two=2&sarv=boo").body)
+
+throwapp() = (_...) -> error("An error!")
+
+# Test production server
+@app test = (
+  Mux.prod_defaults,
+  page("/",respond("<h1>Hello World!</h1>")),
+  page("/about", respond("<h1>Boo!</h1>")),
+  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
+  throwapp(),
+  Mux.notfound())
+serve(test, 8001)
+@test String(HTTP.get("http://localhost:8001").body) ==
+            "<h1>Hello World!</h1>"
+@test String(HTTP.get("http://localhost:8001/about").body) ==
+            "<h1>Boo!</h1>"
+@test String(HTTP.get("http://localhost:8001/user/julia").body) ==
+            "<h1>Hello, julia!</h1>"
+@test String(HTTP.get("http://localhost:8001/badurl";
+                      status_exception=false).body) ==
+             "Internal server error"

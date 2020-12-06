a0786584ce256b8a433d30a5ff8abd13cf40892e
Colin Caine <cmcaine@gmail.com>
2020-12-06 19:56:39 +0000

    Shut the error logger in the tests up
    Also validate that it does return the message we expect.

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,10 +48,14 @@ end
 @test_throws StatusError String(HTTP.get("http://localhost:8000/dum?one=1&two=2&sarv=boo").body)
 
 throwapp() = (_...) -> error("An error!")
+# Used for wrapping stderrcatch so we can check its output and stop it spewing
+# all over the test results.
+path, mock_stderr = mktemp()
 
 # Test production server
 @app test = (
   Mux.prod_defaults,
+  (app, req) -> redirect_stderr(() -> Mux.stderrcatch(app, req), mock_stderr),
   page("/",respond("<h1>Hello World!</h1>")),
   page("/about", respond("<h1>Boo!</h1>")),
   page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
@@ -67,6 +71,9 @@ serve(test, 8001)
 @test String(HTTP.get("http://localhost:8001/badurl";
                       status_exception=false).body) ==
              "Internal server error"
+seekstart(mock_stderr)
+@test occursin("An error!", read(mock_stderr, String))
+rm(path)
 
 # Test page and route are callable without a string argument
 # (previously the first two raised StackOverflowError)

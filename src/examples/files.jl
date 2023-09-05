e43c034811f9002a4f9f1e104fefc880c0dc3d88
Colin Caine <cmcaine@gmail.com>
2023-09-06 00:20:12 +0100

    Document how to use a relative path with `files`
    See also: #26.

--- src/examples/files.jl
+++ src/examples/files.jl
@@ -24,9 +24,35 @@ fresp(f) =
   isdir(f) ?  dirresponse(f) :
   error("$f doesn't exist")
 
-files(root, dirs = true) =
+"""
+    files(root, dirs=true)
+
+Middleware to serve files in the directory specified by the absolute path `root`.
+
+`req[:path]` will be combined with `root` to yield a filepath.
+If the filepath is contained within `root` (after normalisation) and refers to an existing file (or directory if `dirs=true`), then respond with the file (or a directory listing), otherwise call the next middleware.
+
+If you'd like to specify a `root` relative to your current working directory
+or to the directory containing the file that your server is defined in, then
+you can use `pwd()` or `@__DIR__`, and (if you need them) `joinpath` or `normpath`.
+
+# Examples
+
+```
+files(pwd()) # serve files from the current working directory
+files(@__DIR__) # serve files from the directory the script is in
+
+# serve files from the assets directory in the same directory the script is in:
+files(joinpath(@__DIR__, "assets"))
+
+# serve files from the assets directory in the directory above the directory the script is in:
+files(normpath(@__DIR__, "../assets))
+```
+"""
+function files(root, dirs = true)
   branch(req -> validpath(root, joinpath(req[:path]...), dirs=dirs),
          req -> fresp(joinpath(root, req[:path]...)))
+end
 
 # Directories
 
